edda7945cb3f36e3a103d0a4df93f6998e830f4d
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2020-11-19 15:11:14 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class Paper {
+    constructor(x, y, width, height) {
+      var options = {
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+        
+      }
+      this.body =Bodies.circle(200,200,10,options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(4);
+      stroke("purple");
+      fill("purple");
+      ellipseMode(RADIUS);
+      ellipse(pos.x,pos.y,20,20);
+      pop();
+    }
+  };
+  
